46cfead73eda1d5932573d2261ff0c7ed7a59cc1
ramsay <chemyshevat@gmail.com>
2017-05-30 14:22:19 +0600

    gii, admin's modul, models

---
+++ models/Article.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "article".
+ *
+ * @property integer $id
+ * @property string $title
+ * @property string $description
+ * @property string $content
+ * @property string $date
+ * @property string $image
+ * @property integer $viewed
+ * @property integer $user_id
+ * @property integer $status
+ * @property integer $category_id
+ */
+class Article extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'article';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['description', 'content'], 'string'],
+            [['date'], 'safe'],
+            [['viewed', 'user_id', 'status', 'category_id'], 'integer'],
+            [['title', 'image'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'title' => 'Title',
+            'description' => 'Description',
+            'content' => 'Content',
+            'date' => 'Date',
+            'image' => 'Image',
+            'viewed' => 'Viewed',
+            'user_id' => 'User ID',
+            'status' => 'Status',
+            'category_id' => 'Category ID',
+        ];
+    }
+}
